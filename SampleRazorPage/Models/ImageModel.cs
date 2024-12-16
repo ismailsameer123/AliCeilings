dea40165c5aa28626285afba900dbd8dca494865
Mohammed Ismail Sameer <sameer@tenzingtechnologies.com>
2024-12-16 17:22:34 +0530

    Add project files.

---
+++ SampleRazorPage/Models/ImageModel.cs
@@ -0,0 +1,15 @@
+﻿namespace SampleRazorPage.Models
+{
+    public class ImageModel
+    {
+        public string Url { get; set; }
+        public string AltText { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class CarouselViewModel
+    {
+        public List<ImageModel> Images { get; set; }
+    }
+}
